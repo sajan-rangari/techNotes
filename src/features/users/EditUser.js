0e47e44d29de932b1da6f4be4ac32ba3a188b8b2
Sajan Rangari <sajan_rangari@persistent.com>
2022-11-30 17:51:04 +0530

    Ready to deploy

---
+++ src/features/users/EditUser.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useGetUsersQuery } from "./usersApiSlice";
+import EditUserForm from "./EditUserForm";
+import PulseLoader from "react-spinners/PulseLoader";
+
+const EditUser = () => {
+  const { id } = useParams();
+
+  // const user = useSelector((state) => selectUserById(state, id));
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      user: data?.entities[id],
+    }),
+  });
+
+  const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>;
+
+  return content;
+};
+export default EditUser;
