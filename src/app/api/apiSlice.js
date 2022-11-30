4c1ed8f6d56d96f402203b7df2165f5bc4e17c48
Sajan Rangari <sajan_rangari@persistent.com>
2022-11-30 22:00:31 +0530

    frontend url changes

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-at4v.onrender.com",
+  baseUrl: "https://technotes-app.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
